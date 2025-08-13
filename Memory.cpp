c78ed10a466c82b9b8e1529b8f395778ad70e044
Tomke <pfoch.tomke@gmail.com>
2025-08-13 15:43:32 +0200

    Unaligned GC

--- Memory.cpp
+++ Memory.cpp
@@ -4,6 +4,46 @@
 #include <vector>
 
 namespace TTT {
-	auto Memory::alloc() -> Atom * { return heap.get() + (current++); }
-	void Memory::mark(Atom *_){}
+
+	auto Heap::alloc(size_t size) -> std::byte * {
+		allocations.push_back({current});
+		std::byte *start = current+sizeof(GCHeader);
+		current = start + size;
+		return start;
+	}
+	auto Heap::get_chunk(void *ptr) -> Allocation * {
+		if(ptr > end || ptr < data.get()) return nullptr;
+		Allocation *a = allocations.begin().base(), *b = allocations.end().base();
+		while (a-b <= 2) {
+			Allocation *center = a+((b-a)/2);
+			if (ptr > center->start)
+				a = center;
+			else if (ptr < center->start)
+				b = center - 1;
+			else return center;
+		}
+		return a;
+	}
+
+    struct CallMark {
+		template<class T>
+		void operator()(const T&_){}
+
+		template <class T> requires(const T& c){c.mark_children(*this);}
+	void operator()(const T &c) {
+		c.mark_children(*this);
+    }
+		
+    };
+    void Heap::mark(void *ptr) {
+
+		GCHeader *h;
+		if(( h = reinterpret_cast<GCHeader*>(get_chunk(ptr))) == nullptr)
+            return;
+        if (!h->marked) {
+            A
+			
+                }
+        h->marked = true;
+    }
 }
--- Memory.hpp
+++ Memory.hpp
@@ -1,19 +1,68 @@
 #include <cstddef>
+#include <cstdint>
 #include <memory>
+#include <ankerl/unordered_dense.h>
 #include <vector>
 #include "Atom.hpp"
 
 namespace TTT {
 
 
-	class Memory {
-		std::unique_ptr<Atom> heap;
-		size_t current {};
+	struct Allocation {
+		std::byte *start;
+    };
+    struct GCHeader {
+		bool marked;
+    };
+
+    struct GC;
+    
+	// Memory-Aligned Dynamic Storage
+	// Should not contain Refernces to Stack
+    class Heap {
+
+		friend GC;
+        std::unique_ptr<std::byte> data;
+        std::byte *current;
+        std::byte *end;
+        std::vector<Allocation> allocations;
+
+        auto get_chunk(void *ptr) -> Allocation *;
+        void mark(void *);
 	public:
-		auto alloc() -> Atom*;
-		void collect();
-		void mark(Atom *);
-		Memory(size_t initial_size) : heap(new Atom[initial_size]){}
+        auto alloc(size_t bytes) -> std::byte*;
+        template <class T>
+        auto alloc() -> T* {return reinterpret_cast<T*>(alloc(sizeof(T)));}
+
+        Heap(size_t size)
+        : data(new std::byte[size]), current(data.get()), end(current+size)
+        {}
+    };
+
+    // Reference to Argument on Stack for capture
+    struct Argument {
+		uint16_t ascend; 		// Stackframe from top of Stack
+		uint16_t frame_index; 	// Offset within that Frame
+    };
+
+    // Atom-Aligned Argument Storage
+    // Can contain references to Dynamic Memory
+    class Stack {
+        std::unique_ptr<Atom> data;
+        Atom *end;
+		std::vector<Atom *> frame_base;
+        
+    public:
+		Stack(size_t size)
+		: data(new Atom[size]), end(data.get()+size), frame_base({data.get()})
+		{}
+        void push_frame(size_t num_args);
+        
+        // capture Variables for Closure Environments returns location of arg
+        auto capture(Argument arg) -> Atom *;
+
+        // Pop Frame and construct Linked Environemnt if capture required
+        void pop_stack(Heap &store);
 	};
 	
 } // namespace TTT
