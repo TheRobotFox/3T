ab6e0b4b3012e20ae7b8a8f1dc16f083a257f7d7
Tomke <pfoch.tomke@gmail.com>
2025-08-21 17:55:04 +0200

    Atom varaint

--- Atom.hpp
+++ Atom.hpp
@@ -1,227 +1,260 @@
-#pragma once
-#include <ankerl/unordered_dense.h>
-#include <atomic>
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
-#include <functional>
-#include <unordered_map>
-#include <variant>
+#include <ankerl/unordered_dense.h>
+#include <memory>
+#include <type_traits>
+#include "util.hpp"
 
 namespace TTT {
-	using SymbolId = long;
-	class GC;
-	struct Interpreter;
+
+    class Evaluator;
 
 
-	// Literals
-	template <class T> struct LitImpl;
-
-	using	Char	= LitImpl<char>;
-	using	Integer = LitImpl<long long>;
-	using	Real = LitImpl<double>;
-	using	String	= LitImpl<std::string>;
-
-	struct Symbol;
-	struct Quoted;
-
-	struct t;
-	struct nil;
-
-
-	// Containers
-    template <class Fn> struct CallChildren;
-
-	struct Cons;
-	struct HashTable;
-
-
-	// Procedures
-	struct Closure;
-	struct Macro;
-	struct Special;
-
-	
-	// Miscellaneously
-	struct Forward;
-
-	using Atom = std::variant<nil, t, String, Char, Integer, Real, Symbol, Quoted,
-							  Cons, HashTable, Closure, Macro, Special, Forward>; 
-
-	
-
-	using Env = std::unordered_map<SymbolId, Atom *>;
-	struct Cons_iterator;
-
-} // namespace TTT
-using namespace TTT;
-
-template <> struct std::hash<Atom> {
-	using is_avalanching = void;	
-	auto operator()(const Atom &at) -> size_t;
-};
-
-
-
-
-/****************************************************
- * 						Literals					*
- ****************************************************/
-
-
-template <class T> struct TTT::LitImpl {
-	T value;
-	auto operator==(const LitImpl<T> &other) const -> bool {return value == other.value;}
-};
-
-struct TTT::Symbol {
-	SymbolId id;
-	auto operator==(const Symbol &other) const -> bool = default;
-};
- 
-struct TTT::Quoted {
-	SymbolId sym;
-	unsigned int depth;
-	auto operator==(const Quoted &other) const -> bool = default;
-};
-
-template <> struct std::hash<Symbol> {
-	using is_avalanching = void;
-	auto operator()(const Symbol &c)	-> size_t;	
-};
-template <> struct std::hash<Quoted> {
-	using is_avalanching = void;
-	auto operator()(const Quoted &c)	-> size_t;
-};
-
-struct TTT::t {
-	auto operator==(const t &_) const -> bool { return true; }
-};
-struct TTT::nil {
-	auto operator==(const nil&_) const -> bool {return true;}
-};
-
-
-template <class T> struct std::hash<LitImpl<T>> {
-	using is_avalanching = void;
-	auto operator()(const LitImpl<T> &l) -> size_t {
-		return std::hash<T>{}(l.value);
-	}
-};
-template <> struct std::hash<t> {
-	using is_avalanching = void;
-	auto operator()(const t &_)		-> size_t {return 0x589965cc75374cc3;};
-};
-template <> struct std::hash<nil> {
-	using is_avalanching = void;
-	auto operator()(const nil &_)	-> size_t {return 0x8ebc6af09c88c6e3;};
-};
-
-
-
-
-
-/****************************************************
- *					Miscellaneously					*
- ****************************************************/
-
-
-struct TTT::Forward {
-	Atom *reference;
-	auto operator==(const Forward &other) const -> bool;
-};
-
-template <> struct std::hash<Forward> {
-	using is_avalanching = void;
-	auto operator()(const Forward &fw) -> size_t;
-};
-
-
-
-
-/****************************************************
- *					Procedures						*
- ****************************************************/
-
-
-struct TTT::Closure {
-	Atom *body; /* byte code */
-	Env env;
-	std::vector<SymbolId> args;
-	SymbolId rest{0};
-	auto operator==(const Closure &other) const -> bool {return body == other.body;}
-};
-
-struct TTT::Macro {
-	Atom *body;
-	Env env;
-	std::vector<SymbolId> args;
-	SymbolId rest;
-	auto operator==(const Macro	  &other) const -> bool  {return body == other.body;}
-};
-
-struct TTT::Special {
-	std::function<bool(Interpreter &, Env &, Env &, const std::vector<Atom> &, Atom *)> func;
-	Env env;
-	
-	auto operator==(const Special &other) const -> bool {
-		return this == &other;
-	}
-};
-
-template <> struct std::hash<Closure> {
-	using is_avalanching = void;
-	auto operator()(const Closure &c) -> size_t;	
-};
-template <> struct std::hash<Macro> {
-	using is_avalanching = void;
-	auto operator()(const Macro &c)	-> size_t;
-};
-template <> struct std::hash<Special> {
-	using is_avalanching = void;
-	auto operator()(const Special &c) -> size_t;	
-};
-
-
-
-
-
-/****************************************************
- *					Containers						*
- ****************************************************/
-
-template <> struct std::hash<Cons> {
-	using is_avalanching = void;
-	auto operator()(const Cons &c) -> size_t;
-};
-
-template <> struct std::hash<HashTable> {
-	using is_avalanching = void;
-	auto operator()(const HashTable &ht) -> size_t;
-};
-
-struct TTT::Cons {
-	Atom *car, *cdr;
-	auto operator==(const Cons &other) const -> bool;
-};
-
-struct TTT::HashTable {
-	ankerl::unordered_dense::map<Atom, Atom *> value;
-	auto operator==(const HashTable &other) const -> bool;
-};
-template <class Fn> struct TTT::CallChildren {
-	Fn op;
-	template <class T> void operator()(const T &_) const {}
     
-	void operator()(HashTable &ht) const {
-		for (auto &[k, v] : ht.value) {
-			k.visit(*this);
-			op(v);
+	/*
+	 * Atom Types
+	 */
+
+
+	enum Type : Type_t {
+		NIL,
+		T,
+		INT,
+		REAL,
+		STRING,
+		SYMBOL,
+		QUOTED,
+		CONS,
+		TABLE,
+		CLOSURE,
+		MACRO,
+		SPECIAL,
+		ARRAY
+	};
+
+	using Nil = struct{};
+	using t = struct {};
+
+    struct Array;
+
+    struct Quoted {
+		long long sym;
+		long long depth;
+    };
+    struct Cons;
+    struct Closure;
+    using Macro = Closure;
+    struct Special;
+    struct Table;
+
+    template <class... Types> class AtomData;
+    
+    using util::AtomType;
+    using Atom = AtomData < AtomType<NIL, Nil>, AtomType<T, t>,
+						   AtomType<INT, long long>, AtomType<REAL, double>,
+						   AtomType<STRING, std::string>, AtomType<SYMBOL, long long>,
+						   AtomType<QUOTED, Quoted>, AtomType<CONS, Cons>,
+						   AtomType<TABLE, Table>,
+						   AtomType<CLOSURE, Closure>, AtomType<MACRO, Macro>,
+						   AtomType<SPECIAL, Special>,
+						   AtomType<ARRAY, Array>>;
+
+
+
+	
+
+	// Compact Header tries uses Bit-Ops to fit fields into single Byte
+	template<size_t TypeCount>
+	class CompactHeader {
+        size_t data{markBit};
+
+        constexpr static const size_t typeBits = util::repBits(TypeCount);
+
+        constexpr static const size_t typeMask =
+            (1 << typeBits) - 1;
+        constexpr static const size_t markBit =
+            1 << (typeBits + 1);
+        constexpr static const size_t processedBit =
+            1 << (typeBits + 2);
+        constexpr static const size_t refBit =
+            1 << (typeBits + 3);            
+        
+	public:
+		
+		constexpr auto getType() const -> Type_t	{return		 data &	 typeMask	  ;}
+        constexpr void setType(Type_t t)    		{data = (data & ~typeMask) | t;}
+
+
+		constexpr auto isRef() const	 -> Bool_t {return data & refBit;}
+		constexpr void setRef() 		 	  	   {data |=  refBit;}
+		constexpr void unsetRef() 		 	  	   {data &= ~refBit;}
+        
+		constexpr auto isMarked() const -> Bool_t { return data & markBit; }
+		constexpr void mark() {data|= markBit;}
+		constexpr void unmark() {data &= ~markBit;}
+
+        constexpr auto isProcessed() const -> Bool_t {return data & processedBit;  }
+		constexpr void setProcessed() 				 {data |= 	processedBit; }
+		constexpr void unsetProcessed() 			 {data &=  ~processedBit; }
+
+		constexpr CompactHeader(Type_t type, bool ref = false) {
+			setType(type);
+			if (ref) setRef();
 		}
-	}
-	void operator()(Cons &c) const {
-		op(c.car);
-		op(c.cdr);
+	};
+
+
+	
+    
+	// Select Header to use
+	template<size_t TypeCount>
+	using use_header = CompactHeader<TypeCount>;
+
+
+    
+	// Variant-esque Implementation of Generic Box
+	template <class... Types>
+	class AtomData : private Types..., public use_header<sizeof...(Types)> {
+        static_assert(util::_check_ids_well_ordered<Types...>(),
+                        "TypeIds must form Interval 0..n!");
+	public:
+        using header = use_header<sizeof...(Types)>;
+        template <size_t id>
+        using Type = Types...[id];
+
+		template <class E, size_t id = 0>
+		static constexpr auto type_id() -> size_t {
+			if constexpr (id>=sizeof...(Types)) return id;
+			else if constexpr (std::is_same_v<typename Types...[id] ::Type, E>)
+				return id;
+			else return type_id<E, id+1>();
+		}
+
+		
+		// Impl visit
+		template <class Fn> auto visit(Fn &&fn) {
+			using Result = decltype(fn((Types...[0]{})));
+			static constexpr std::array<Result (*)(Fn&&, AtomData<Types...> &), sizeof...(Types)> vtable = {&invoke<Fn, Types::Id> ...};
+			return vtable[header::getType()](std::forward<Fn>(fn), *this);
+        }
+
+		template <template <size_t> class Visitor, class... Args> auto visitId(Args &&...args) {
+			using Result = decltype(Visitor<0>::operator()(args...));
+
+			static constexpr std::array<Result (*)(Args...), sizeof...(Types)> vtable = {&Visitor<Types::Id>::operator() ...};
+			return vtable[header::getType()](args...);
+		}
+
+        template <size_t id>
+		auto get_unchecked() -> util::index_pack<id, Types...>::Type::Type & { // G++ Struggels with mangleing type-parameter Pack indexing
+			static_assert(id<sizeof...(Types), "TypeId out of Range!");
+			if (this->isRef())
+				return *reinterpret_cast<Types...[id]::Type *>(data[0]);
+			return *reinterpret_cast<Types...[id]::Type*>(&data);
+		}
+
+     	template <size_t id>
+		auto get() -> Types... [id] ::Type* {
+			if (header::getType() != id)
+				return nullptr;
+			return &get_unchecked<id>();
+		}
+		
+		template <size_t id>
+		auto size_of() -> size_t {
+			assert(header::getType() != id && "AtomData holds different Type, could not obtain size!");
+			return Types...[id]::size_of(get_unchecked<id>())+sizeof(*this);
+		}
+
+	private:
+
+		void* data[0];
+
+
+		// dispatcher for VTable
+		template <class Fn, size_t id>
+		static decltype(auto) invoke(Fn &&fn, AtomData &ad) {
+			// if constexpr (std::is_same_v<typename Types...[id] ::Type, void>)
+			//	exit(-1);
+			// else
+			return std::invoke(fn, ad.get_unchecked<id>());
+        }
+	public:
+          template <size_t id>
+		  AtomData(Types...[id] ::Type &&data) : header(id) {
+			  using T = Types...[id] ::Type;
+			  static_assert(id < sizeof...(Types),
+								"Type is not part of Atom, failed to "
+								"retrieve TypeId!");
+		
+			  std::construct_at(reinterpret_cast<T *>(&this->data), data);
+          }
+        
+		template <size_t id>
+		AtomData(Types...[id] ::Type *data) : header(id, true) {
+			using T = Types...[id] ::Type;
+			static_assert(id < sizeof...(Types),
+								"Type is not part of Atom, failed to "
+								"retrieve TypeId!");
+			
+			*reinterpret_cast<T**>(&this->data[0]) = data;
+		}
+		AtomData(header h) : header(h){}
+		AtomData(header &&h, size_t length);
+	};
+
+
+
+	// No arrays of refs
+	struct Array {
+		template <size_t id> struct CallSizeOf {
+            static auto operator()(Atom &elements) -> size_t {
+				if(elements.isRef()!=0) return sizeof(void*);
+                return Atom::Type<id>::size_of(elements.get_unchecked<id>());}
+        };
+
+		const size_t length;
+        Atom elements;
+		template <size_t id> auto at(size_t idx) -> Atom::Type<id>::Type & {
+            using T = Atom::Type<id>::Type;
+            assert(!elements.isRef());
+			return *(&elements.get_unchecked<id>()+idx);
+		}
+		template<size_t id>
+		auto size_of() -> size_t {
+            return sizeof(Array) +
+              (length * elements.visitId<CallSizeOf>(elements));
+        }
+        Array(size_t length, Atom::header h): length(length), elements(h){}
+    };
+    template <class... Types>
+	AtomData<Types...>::AtomData(header &&h, size_t length) : header(ARRAY) {
+
+		auto *a = reinterpret_cast<Array *>(&this->data[0]);
+		std::construct_at(a, length, h);
 	}
 
-    CallChildren(const Fn &fn) : op(fn){}
-};
+	    struct Cons {
+          Atom *car, *cdr;
+        };
+    struct Table {
+        ankerl::unordered_dense::map<Atom, Atom*> val;
+    };
+    
+	using Environment =	ankerl::unordered_dense::map<long long, Atom *>;
+
+	struct Closure {
+		Environment env;
+		long long expect_args; // negative = rest args
+		void *body;
+	};
+	using Macro = Closure;
+    struct Special {
+        std::function<bool(Evaluator &, Environment &,
+						Atom *, size_t)> func;
+		
+    };
+
+	
+}
--- Memory.hpp
+++ Memory.hpp
@@ -3,33 +3,27 @@
 #include <memory>
 #include <ankerl/unordered_dense.h>
 #include <span>
-#include <thread>
 #include <vector>
-#include "Atom.hpp"
+#include "GC.hpp"
 
 namespace TTT {
 
-	class GC;
-	#define UNMARKED 0
+	class Buffer {
 
-
-        // Atom-Aligned Dynamic Storage
-    // Should not contain Refernces to Stack
-    template<class GC>
-	class Heap {
-
-		friend GC;
-        std::unique_ptr<typename GC::Cell[]> start;
-        GC::Cell *current, *end;
+        friend use_gc;
+        friend use_marker;
         
-			void mark(Atom *);
-			auto alloc() -> Atom *;
+        std::unique_ptr<Cell[]> start;
+        Cell *current, *end;
+        
+		void mark(Atom *);
+		auto alloc() -> Atom *;
 
-		[[nodiscard]] auto span() const -> std::span<typename GC::Cell> {return std::span{start.get(), current};}
+		[[nodiscard]] auto span() const -> std::span<Cell> {return std::span{start.get(), current};}
 
     public:
         [[nodiscard]] auto available() const -> size_t;
-		Heap(size_t size) : start(new typename GC::C[size]), current(start.get()), end(start.get()+size)
+		Buffer(size_t size) : start(new Cell[size]), current(start.get()), end(start.get()+size)
 		{}
 	};
 
@@ -40,73 +34,25 @@ namespace TTT {
 		uint16_t frame_index;	// Offset within that Frame
 	};
 
-        // Atom-Aligned Argument Storage
-    // Can contain references to Dynamic Memory
-    template<class GC>    
-	class Stack {
-		friend GC;
-		
-		std::unique_ptr<Atom[]> data;
+    class Stack {
+
+		friend use_gc;
+        friend use_marker;
+        
+		std::unique_ptr<Atom[]> start;
 		Atom *current;
 		Atom *end;
 		std::vector<Atom *> frame_base;
-		
-	public:
+
+    public:
+        
 		Stack(size_t size)
-		: data(new Atom[size]), current(data.get()), end(data.get()+size), frame_base({data.get()})
+		: start(new Atom[size]), current(start.get()), end(start.get()+size), frame_base({start.get()})
 		{}
         void end_frame();
         auto push() -> Atom&;
 		
-		// capture Variables for Closure Environments returns location of arg
-		auto capture(Argument arg, Heap<GC> &store) -> Atom *;
 		void drop_frame();
 	};
-
-    class CompactGC {
-
-		static constexpr size_t chunk_size = 1 << 20;
-		
-        struct Pass {
-            size_t pass_nr, count, offset;
-            std::array<Cell *, chunk_size> chunk;
-            auto get_offset(Cell *atom) const -> size_t;
-			void flush(Heap<CompactGC> &heap);
-			void add(Cell *dead) {
-				chunk[count++] = dead;
-			}
-		};
-
-        void do_pass(const Pass &p);
-
-    public:
-        struct Cell {
-			Atom atom;
-			uint_fast16_t pass_nr;
-        };
-
-        Stack<CompactGC> stack;
-   		Heap<CompactGC> heap;
-
-		// std::set<Atom*> mark; TODO
-		auto collect() -> size_t;
-        auto alloc() -> Atom *;
-        
-		CompactGC(Stack<CompactGC> &&stack, Heap<CompactGC> &&h)
-		: stack(std::move(stack)), heap(std::move(h))
-		{}
-    };
-
-    class ConcurrentGC {
-		std::thread t;
-	public:
-      struct Cell {
-          Atom atom;
-          bool marked;
-      };
-		size_t threshold;
-		auto collect() -> size_t;
-		auto alloc() -> Atom *;
-    };
 	
 } // namespace TTT
